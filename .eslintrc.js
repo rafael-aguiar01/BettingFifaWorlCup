8520c8f0636fdc8e4982f83f5ebaf19bfb2a3c75
Rafael Aguiar <rafael.a1@hotmail.com>
2022-10-17 08:18:10 -0300

    chore: configure jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,12 @@ module.exports = {
   extends: 'standard-with-typescript',
   parserOptions: {
     project: './tsconfig.json'
+  },
+  rules: {
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/method-signature-style': 'off',
+    '@typescript-eslint/return-await': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/space-before-blocks': 'off'
   }
 }
