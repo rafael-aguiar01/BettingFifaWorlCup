1ae2848feebce8cc2e40c155630a0678159f6c03
Rafael Aguiar <rafael.a1@hotmail.com>
2022-10-17 09:32:05 -0300

    test: ensure AddNationController return an error if no email is provided

--- src/presentation/controllers/nation/add-nation.spec.ts
+++ src/presentation/controllers/nation/add-nation.spec.ts
@@ -10,6 +10,17 @@ describe('AddNation Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: code'))
+  })
+  test('Should return 400 if no name is provided', () => {
+    const sut = new AddNationController()
+    const httpRequest = {
+      body: {
+        code: 'any_code'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 })
