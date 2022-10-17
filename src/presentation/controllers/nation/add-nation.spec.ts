493fb67a4bb7e13c4e3843d76b7eee4f4d3cbe1f
Rafael Aguiar <rafael.a1@hotmail.com>
2022-10-17 15:18:07 -0300

    refactor: move errors to errors interface

--- src/presentation/controllers/nation/add-nation.spec.ts
+++ src/presentation/controllers/nation/add-nation.spec.ts
@@ -1,4 +1,6 @@
 import { AddNationController } from './add-nation'
+import { MissingParamError } from '../../errors'
+import { badRequest } from '../../helpers/http-helper'
 
 describe('AddNation Controller', () => {
   test('Should return 400 if no code is provided', () => {
@@ -10,7 +12,7 @@ describe('AddNation Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: code'))
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('code')))
   })
   test('Should return 400 if no name is provided', () => {
     const sut = new AddNationController()
@@ -21,6 +23,6 @@ describe('AddNation Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('name')))
   })
 })
--- src/presentation/controllers/nation/add-nation.ts
+++ src/presentation/controllers/nation/add-nation.ts
@@ -1,17 +1,13 @@
 import { HttpResponse, HttpRequest } from '../../protocols/http'
+import { MissingParamError } from '../../errors'
+import { badRequest } from '../../helpers/http-helper'
 
 export class AddNationController {
   handle (httpRequest: HttpRequest): HttpResponse{
-    if (!httpRequest.body.code){
-      return {
-        statusCode: 400,
-        body: new Error('Missing param: code')
-      }
-    }
-    if (!httpRequest.body.name){
-      return {
-        statusCode: 400,
-        body: new Error('Missing param: name')
+    const requiredFields = ['code', 'name']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
       }
     }
   }
