2fc17cae6ee69abc643c5157f7ed4ffc03b17cd2
Rafael Aguiar <rafael.a1@hotmail.com>
2022-10-25 07:31:36 -0300

    test: the DbAddGroup UseCase should call AddRepository with correct values

---
+++ src/data/usecases/add-group/db-add-group.spec.ts
@@ -0,0 +1,58 @@
+import { GroupModel } from '../../../domain/models/group'
+import { AddGroupModel } from '../../../domain/usecases/add-group'
+import { AddGroupRepository } from '../../protocols/add-group-repository'
+import { DbAddGroup } from './db-add-group'
+
+const makeAddGroupRepository = (): AddGroupRepository => {
+  class AddGroupRepositoryStub implements AddGroupRepository {
+    async add (groupData: AddGroupModel): Promise<GroupModel> {
+      return new Promise(resolve => resolve(makeFakeGroup()))
+    }
+  }
+  return new AddGroupRepositoryStub()
+}
+
+const makeFakeGroup = (): GroupModel => ({
+  code: 'valid_code',
+  teamA: 'valid_teamA',
+  teamB: 'valid_teamB',
+  teamC: 'valid_teamC',
+  teamD: 'valid_teamD'
+})
+
+const makeFakeNationData = (): AddGroupModel => ({
+  code: 'valid_code',
+  teamA: 'valid_teamA',
+  teamB: 'valid_teamB',
+  teamC: 'valid_teamC',
+  teamD: 'valid_teamD'
+})
+
+interface SutTypes {
+  sut: DbAddGroup
+  addGroupRepositoryStub: AddGroupRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addGroupRepositoryStub = makeAddGroupRepository()
+  const sut = new DbAddGroup(addGroupRepositoryStub)
+  return {
+    sut,
+    addGroupRepositoryStub
+  }
+}
+
+describe('DbAddGroup Usecase', () => {
+  test('Should call AddGroupRepository with correct values', async () => {
+    const { sut, addGroupRepositoryStub } = makeSut()
+    const addSpy = jest.spyOn(addGroupRepositoryStub, 'add')
+    await sut.add(makeFakeNationData())
+    expect(addSpy).toHaveBeenCalledWith({
+      code: 'valid_code',
+      teamA: 'valid_teamA',
+      teamB: 'valid_teamB',
+      teamC: 'valid_teamC',
+      teamD: 'valid_teamD'
+    })
+  })
+})
