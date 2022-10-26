21e7e394a80c44e3df60f94a9f4b0f94f7c2e687
Rafael Aguiar <rafael.a1@hotmail.com>
2022-10-26 10:06:44 -0300

    feat: ensure MatchMongoRepository return an match on success

---
+++ src/infra/db/mongodb/match-repository/match.spec.ts
@@ -0,0 +1,40 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { MatchMongoRepository } from './match'
+
+describe('Match Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const nationCollection = await MongoHelper.getCollection('matches')
+    await nationCollection.deleteMany({})
+  })
+
+  const makeSut = (): MatchMongoRepository => {
+    return new MatchMongoRepository()
+  }
+
+  test('Should return an match on success', async () => {
+    const sut = makeSut()
+    const match = await sut.add({
+      code: 'any_code',
+      teamA: 'any_teamA',
+      scoreTeamA: 1,
+      teamB: 'any_teamB',
+      scoreTeamB: 2,
+      winner: 'valid_winner'
+    })
+    expect(match).toBeTruthy()
+    expect(match.code).toBe('any_code')
+    expect(match.teamA).toBe('any_teamA')
+    expect(match.scoreTeamA).toBe(1)
+    expect(match.teamB).toBe('any_teamB')
+    expect(match.scoreTeamB).toBe(2)
+    expect(match.winner).toBe('valid_winner')
+  })
+})
