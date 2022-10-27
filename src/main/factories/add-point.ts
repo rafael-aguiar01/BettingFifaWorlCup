557b23d8bbc9cbf523794cf00c1ae5db2cc4d3d2
Rafael Aguiar <rafael.a1@hotmail.com>
2022-10-27 16:23:55 -0300

    feat: make factory add-point

---
+++ src/main/factories/add-point.ts
@@ -0,0 +1,11 @@
+import { AddPointController } from '../../presentation/controllers/point/add-point'
+import { DbAddPoint } from '../../data/usecases/add-point/db-add-point'
+import { PointMongoRepository } from '../../infra/db/mongodb/point-repository/point'
+import { Controller } from '../../presentation/protocols'
+
+export const makeAddPointontroller = (): Controller => {
+  const pointMongoRepository = new PointMongoRepository()
+  const dbAddPoint = new DbAddPoint(pointMongoRepository)
+  const addPointController = new AddPointController(dbAddPoint)
+  return addPointController
+}
