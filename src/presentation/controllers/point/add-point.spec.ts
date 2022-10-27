4ff4263b00d22b454f51f8b812898d6ce7257bb4
Rafael Aguiar <rafael.a1@hotmail.com>
2022-10-27 14:47:29 -0300

    test: ensure AddPoint Controller return 400 if no code is provided

---
+++ src/presentation/controllers/point/add-point.spec.ts
@@ -0,0 +1,47 @@
+import { AddPointController } from './add-point'
+import { MissingParamError } from '../../errors'
+import { badRequest } from '../../helpers/http-helper'
+import { PointModel } from '../../../domain/models/point'
+import { AddPoint, AddPointModel } from '../../../domain/usecases/add-point'
+
+const makeFakePoint = (): PointModel => ({
+  code: 'valid_code',
+  point: 2
+})
+
+const makeAddPoint = (): AddPoint => {
+  class AddPointStub implements AddPoint {
+    async add (point: AddPointModel): Promise<PointModel> {
+      return new Promise(resolve => resolve(makeFakePoint()))
+    }
+  }
+  return new AddPointStub()
+}
+
+interface SutTypes{
+  sut: AddPointController
+  addPointStub: AddPoint
+}
+
+const makeSut = (): SutTypes => {
+  const addPointStub = makeAddPoint()
+  const sut = new AddPointController(addPointStub)
+  return {
+    sut,
+    addPointStub
+  }
+}
+
+describe('AddPoint Controller', () => {
+  test('Should return 400 if no code is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        point: 2
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('code')))
+  })
+})
