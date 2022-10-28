9632808471a01ecfd1bbea182f3ac4ae60199e87
Rafael Aguiar <rafael.a1@hotmail.com>
2022-10-28 08:05:03 -0300

    test: ensure UpdateMatch Controller return 400 if no code is provided

---
+++ src/presentation/controllers/match/update-match.spec.ts
@@ -0,0 +1,50 @@
+import { UpdateMatchController } from './update-match'
+import { MissingParamError } from '../../errors'
+import { badRequest } from '../../helpers/http-helper'
+import { UpdateMatch, UpdateMatchModel } from '../../../domain/usecases/update-match'
+
+const makeFakeMatch = (): UpdateMatchModel => ({
+  code: 'valid_code',
+  scoreTeamA: 2,
+  scoreTeamB: 2,
+  winner: 'valid_winner'
+})
+
+const makeUpdateMatch = (): UpdateMatch => {
+  class UpdateMatchStub implements UpdateMatch {
+    async update (match: UpdateMatchModel): Promise<UpdateMatchModel> {
+      return new Promise(resolve => resolve(makeFakeMatch()))
+    }
+  }
+  return new UpdateMatchStub()
+}
+
+interface SutTypes {
+  sut: UpdateMatchController
+  updateMatchStub: UpdateMatch
+}
+
+const makeSut = (): SutTypes => {
+  const updateMatchStub = makeUpdateMatch()
+  const sut = new UpdateMatchController(updateMatchStub)
+  return {
+    sut,
+    updateMatchStub
+  }
+}
+
+describe('UpdateMatch Controller', () => {
+  test('Should return 400 if no code is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        scoreTeamA: 2,
+        scoreTeamB: 2,
+        winner: 'valid_winner'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('code')))
+  })
+})
