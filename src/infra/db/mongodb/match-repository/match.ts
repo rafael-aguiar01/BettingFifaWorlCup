19e551434b36278cc853ac351903d5275ccaa6f9
Rafael Aguiar <rafael.a1@hotmail.com>
2022-10-28 11:45:45 -0300

    feat: implement update in match-repository

--- src/infra/db/mongodb/match-repository/match.ts
+++ src/infra/db/mongodb/match-repository/match.ts
@@ -1,6 +1,7 @@
 import { AddMatchRepository } from '../../../../data/protocols/add-match-repository'
 import { MatchModel } from '../../../../domain/models/match'
 import { AddMatchModel } from '../../../../domain/usecases/add-match'
+import { UpdateMatchModel } from '../../../../domain/usecases/update-match'
 import { MongoHelper } from '../helpers/mongo-helper'
 
 export class MatchMongoRepository implements AddMatchRepository {
@@ -8,5 +9,11 @@ export class MatchMongoRepository implements AddMatchRepository {
     const matchCollection = await MongoHelper.getCollection('matches')
     const result = await matchCollection.insertOne(matchData)
     return MongoHelper.map(result.ops[0])
+  }
+
+  async update (matchData: UpdateMatchModel): Promise<MatchModel> {
+    const matchCollection = await MongoHelper.getCollection('matches')
+    const result = await matchCollection.findOneAndUpdate({ code: matchData.code }, { scoreTeamA: matchData.scoreTeamA, scoreTeamB: matchData.scoreTeamB, winner: matchData.winner })
+    return MongoHelper.map(result.value)
   }
 }
