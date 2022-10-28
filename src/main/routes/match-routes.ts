f42686f271b904e1413a70f58d2c7a3f71bcd9e1
Rafael Aguiar <rafael.a1@hotmail.com>
2022-10-28 13:44:51 -0300

    feat: create route of match

--- src/main/routes/match-routes.ts
+++ src/main/routes/match-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { makeAddMatchController } from '../factories/add-match'
 import { adaptRoute } from '../adapters/express-route-adapter'
+import { makeUpdateMatchController } from '../factories/update-match'
 
 export default (router: Router): void => {
   router.post('/match', adaptRoute(makeAddMatchController()))
+  router.patch('/match', adaptRoute(makeUpdateMatchController()))
 }
