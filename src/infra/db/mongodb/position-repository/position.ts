d0c070ff375dd0f70151044b35d9a088ee819169
Rafael Aguiar <rafael.a1@hotmail.com>
2022-11-10 09:58:07 -0300

    feat: ensure PositionMongoRepository update the position

--- src/infra/db/mongodb/position-repository/position.ts
+++ src/infra/db/mongodb/position-repository/position.ts
@@ -1,6 +1,7 @@
 import { AddPositionRepository } from '../../../../data/protocols/add-position-repository'
 import { PositionModel } from '../../../../domain/models/position'
 import { AddPositionModel } from '../../../../domain/usecases/add-position'
+import { UpdatePositionModel } from '../../../../domain/usecases/update-position'
 import { MongoHelper } from '../helpers/mongo-helper'
 
 export class PositionMongoRepository implements AddPositionRepository {
@@ -8,5 +9,14 @@ export class PositionMongoRepository implements AddPositionRepository {
     const positionCollection = await MongoHelper.getCollection('positions')
     const result = await positionCollection.insertOne(positionData)
     return MongoHelper.map(result.ops[0])
+  }
+
+  async update (positionData: UpdatePositionModel): Promise<PositionModel>{
+    const matchCollection = await MongoHelper.getCollection('positions')
+    const result = await matchCollection.findOneAndUpdate(
+      { code: positionData.code },
+      { $set: { code: positionData.code, first: positionData.first, second: positionData.second, third: positionData.third, fourth: positionData.fourth } }
+    )
+    return MongoHelper.map(result.value)
   }
 }
