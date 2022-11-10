9e6510267f5eb101785131738b664cbe59c708c1
Rafael Aguiar <rafael.a1@hotmail.com>
2022-11-10 09:38:25 -0300

    test: ensure DbUpdate Position UseCase should throw if UpdatePositionRepository throws

--- src/data/usecases/update-position/db-update-position.spec.ts
+++ src/data/usecases/update-position/db-update-position.spec.ts
@@ -1,4 +1,3 @@
-import { PositionModel } from '../../../domain/models/position'
 import { UpdatePositionRepository } from '../../protocols/update-position-repository'
 import { UpdatePositionModel } from '../../../domain/usecases/update-position'
 import { DbUpdatePosition } from './db-update-position'
@@ -6,19 +5,11 @@ import { DbUpdatePosition } from './db-update-position'
 const makeUpdatePositionRepository = (): UpdatePositionRepository => {
   class UpdatePositionRepositoryStub implements UpdatePositionRepository {
     async update (positionData: UpdatePositionModel): Promise<UpdatePositionModel> {
-      return new Promise(resolve => resolve(makeFakePosition()))
+      return new Promise(resolve => resolve(makeFakeUpdatePositionData()))
     }
   }
   return new UpdatePositionRepositoryStub()
 }
-
-const makeFakePosition = (): PositionModel => ({
-  code: 'valid_code',
-  first: 'valid_first',
-  second: 'valid_second',
-  third: 'valid_third',
-  fourth: 'valid_fourth'
-})
 
 const makeFakeUpdatePositionData = (): UpdatePositionModel => ({
   code: 'valid_code',
@@ -54,5 +45,18 @@ describe('DbUpdate Position Usecase', () => {
       third: 'valid_third',
       fourth: 'valid_fourth'
     })
+  })
+
+  test('Should throw if UpdatePositionRepository throws', async () => {
+    const { sut, updatePositionRepositoryStub } = makeSut()
+    jest.spyOn(updatePositionRepositoryStub, 'update').mockResolvedValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.update(makeFakeUpdatePositionData())
+    await expect(promise).rejects.toThrow()
+  })
+
+  test('Should return an position updated on success', async () => {
+    const { sut } = makeSut()
+    const account = await sut.update(makeFakeUpdatePositionData())
+    expect(account).toEqual(makeFakeUpdatePositionData())
   })
 })
